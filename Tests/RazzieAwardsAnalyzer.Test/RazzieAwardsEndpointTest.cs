66a123de03beb5190c0fd76b0e843ba20a92c42f
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-01-30 12:21:40 -0300

    Teste de integração para validação de retorno da api

---
+++ Tests/RazzieAwardsAnalyzer.Test/RazzieAwardsEndpointTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using RazzieAwardsAnalyzer.Application.DTOs.Response;
+using System.Net;
+
+namespace RazzieAwardsAnalyzer.Test
+{
+    public class RazzieAwardsEndpointTest: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public RazzieAwardsEndpointTest(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Check_Return_Data_Get_Endpoint()
+        {
+            //Arrange
+            var response = await _client.GetAsync("/api/razzieawards");
+            
+
+            //Act
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseData = JsonConvert.DeserializeObject<AwardIntervalResponseDTO>(responseString);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(responseData);
+        }
+    }
+}
