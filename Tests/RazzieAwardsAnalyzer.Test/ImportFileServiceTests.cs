44956f7a4fd7a03ca8d53b288a89bcecbb204111
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-01-30 09:35:29 -0300

    Adicionado testes para validar consistência de dados importados.

--- Tests/RazzieAwardsAnalyzer.Test/ImportFileServiceTests.cs
+++ Tests/RazzieAwardsAnalyzer.Test/ImportFileServiceTests.cs
@@ -32,5 +32,62 @@ namespace RazzieAwardsAnalyzer.Test
             Assert.NotEmpty(importedData);
         }
 
+        [Fact]
+        public async Task Check_Int_Year_Data_Consistency()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                              .UseInMemoryDatabase(databaseName: "RazzieAwardsDBTest")
+                              .Options;
+            using var context = new ApplicationDbContext(options);
+
+            var _razzieAwardRepository = new RazzieAwardRepository(context);
+            var fileRepository = new FileRepository(Path.Combine(AppContext.BaseDirectory, "FileCSV"));
+            var _importFileService = new ImportFileService(fileRepository, _razzieAwardRepository);
+
+            //Act
+            await _importFileService.ImportFileAsync();
+            var importedData = context.Movies.FirstOrDefault();
+
+            //Assert
+            Assert.True(IsValidYear(importedData?.Year));
+        }
+
+        [Fact]
+        public async Task Check_Bool_Winner_Data_Consistency()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                              .UseInMemoryDatabase(databaseName: "RazzieAwardsDBTest")
+                              .Options;
+            using var context = new ApplicationDbContext(options);
+
+            var _razzieAwardRepository = new RazzieAwardRepository(context);
+            var fileRepository = new FileRepository(Path.Combine(AppContext.BaseDirectory, "FileCSV"));
+            var _importFileService = new ImportFileService(fileRepository, _razzieAwardRepository);
+
+            //Act
+            await _importFileService.ImportFileAsync();
+            var importedData = context.Movies.ToList();
+            int countWinnerFalse = importedData.Where(x => x.Winner == false).Count();
+            int countWinnerTrue  = importedData.Where(x => x.Winner == true).Count();
+            //Assert
+            Assert.True(countWinnerFalse > 0 && countWinnerTrue > 0);
+        }
+        private static bool IsValidYear(int? year)
+        {
+            try
+            {
+                if(year == null)
+                    return false;
+
+                DateTime date = new (year.Value, 1, 1);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
